365073d0b1758ed5d25165942bb9d2227e70acde
Radovan Delic <infamouszgbgd@yahoo.com>
2017-10-30 18:43:14 +0100

    stuff

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 var express = require('express');
 var app = express();
-var allRoutes = require('./routes');
 var path = require('path');
-app.use(allRoutes);
 
+// set up the views directory
+app.set("views", path.join(__dirname,"../views"));
+// set up our view engine
+app.set("view engine", "ejs");
 
-var admin = require('')
-var user
-var guest
+var auth = require("./micro-services/auth");
+app.use("/auth", auth);
 
-app.use(function(req, res) {
-	res.send('404');
+var guest = require("./micro-services/guest");
+app.use("/", (req, res) => {
+	res.redirect("/auth/login");
 })
+
+app.use("/", guest);
+
+
+app.use(function (req, res) {
+	res.send('404');
+});
+
 
 app.listen(3000, (err) => {
 	if (err) {
 		console.log(err);
 	} else {
-		console.log('3000');
+		console.log('using port 3000');
 	};
 });
